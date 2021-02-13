9e190b2847fb7f64225729c05a703c4a9a93a2a9
Solomon1Levy23 <solomon.l@outlook.com>
2021-02-12 19:47:52 -0500

    initial commit

---
+++ CSHARP_APP/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSHARP_APP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+        }
+        
+    }
+}
